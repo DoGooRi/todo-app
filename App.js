69aefbeedfb3fb41f6c6759c8fb0890b81336e61
정현호 <jhh@ancle.kr>
2018-04-25 09:39:28 +0900

    메인 UI 작업 됨

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Dimensions, Platform } from 'react-native';
+
+const { height, width } = Dimensions.get("window")
+
+import { TextInput } from 'react-native-gesture-handler';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar barStyle="light-content" backgroundColor="#0F72DF" />
+        <Text style={styles.title}>오늘의 할일</Text>
+        <View style={styles.card}>
+          <TextInput underlineColorAndroid="transparent" style={styles.input} placeholder={"해야할 일"} />
+        </View>
       </View>
     );
   }
@@ -14,8 +22,37 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0F72DF',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  title: {
+    color: '#fff',
+    fontSize: 30,
+    marginTop: 60,
+    fontWeight: "100",
+    marginBottom: 30
+    
+  },
+  card: {
+    backgroundColor: '#fff',
+    flex: 1,
+    width: width - 25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgb(50,50,50)",
+        shadowOpacity: 0.5,
+        shadowOffset: {
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 3
+      }
+    })
+  },
+  input: {
+  }
 });
